e31fc24007f168420373ab3a72abeb5eacc2a751
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2023-01-25 20:33:39 +0530

    Create functionptr.cpp

---
+++ functionptr.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+#include<ctype.h>
+#include<cstring>
+#include <cfloat>
+using namespace std;
+
+int mul(int *a, int *b){
+    return(*a * *b);
+}
+
+int add(int(*p)(int*, int*), int* x){
+    int value = (*p)(x, x);
+    return(value+*x);
+
+} 
+
+int main()
+{
+    int a = 5, b = 10, result;
+    int (*p)(int*, int*);
+    p = mul;
+    result = add(mul, &b);
+    cout<<result;
+}
