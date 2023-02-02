5bb71d796bf3296b8e09314b9c4efd214e8d2b7a
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2023-02-02 16:48:43 +0530

    Create sumofdivisor.cpp

---
+++ Fundamentalsofdatastruct/sumofdivisor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<math.h>
+#include<ctype.h>
+#include<cstring>
+#include <cfloat>
+#define SWAP(x, y, t)  ((t)=(x), (x)=(y), (y)=(t))
+#define COMPARE(x, y) (((x)<(y))?-1:((x)==(y))?0:1)
+
+int ye(int);
+
+int main()
+{
+    for(int i=2; i<100000; i++){
+        if(ye(i)){
+            printf("%d-%d \n", i, ye(i));
+        }
+    }
+}
+
+int ye(int x){
+    int count = 0;
+    int sum = 0;
+    for(int i=1; i<x; i++){
+        if(x%i==0){
+            sum +=i;
+        }
+    }
+    // for(int j = 0; j<1000; j++){
+    //     sum += divisor[j];
+    // }
+    if(sum==x){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
