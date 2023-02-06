2f5fe07d232b6658918152e34a3cb0c128e600e5
rakesh-i <57840815+rakesh-i@users.noreply.github.com>
2023-02-06 10:58:36 +0530

    Update powerset.cpp

--- Fundamentalsofdatastruct/powerset.cpp
+++ Fundamentalsofdatastruct/powerset.cpp
@@ -1,3 +1,4 @@
+//leetcode 78
 #include<iostream>
 #include<math.h>
 #include<ctype.h>
@@ -8,7 +9,7 @@
 #define SWAP(x, y, t)  ((t)=(x), (x)=(y), (y)=(t))
 #define NOT(x) (((x)<1)?1:0) 
 using namespace std;
-
+//iterative method
 void subset(vector<vector<int>> &a, vector<int> &b){
     a = {{}};
     for(int i=0; i<b.size(); i++){
@@ -19,6 +20,32 @@ void subset(vector<vector<int>> &a, vector<int> &b){
             temp.push_back(b[i]);
             a.push_back(temp);
         }
+    }
+}
+//recursive method
+void powset(vector<vector<int>> &v, vector<int> &b, int i){
+    if(i==b.size()){
+        v.push_back({});
+    }
+    else{
+        subset(v, b, i+1);
+        int n = v.size();
+        //cout<<n<<endl;
+        for(int j=0; j<n; j++){
+            vector<int>temp;
+            temp = v[j];
+            temp.push_back(b[i]);
+            v.push_back(temp);
+        } 
+    }
+}
+
+void print(vector<vector<int>> v){
+    for(int i=0; i<v.size(); i++){
+        for(int j=0; j<v[i].size(); j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
     }
 }
 
